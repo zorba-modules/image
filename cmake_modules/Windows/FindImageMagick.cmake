0770be82cb7c749688ac09450c9874cb1ea71b13
juanza <>
2011-08-19 00:03:49 +0000

    Latest Windows installer updates

--- cmake_modules/Windows/FindImageMagick.cmake
+++ cmake_modules/Windows/FindImageMagick.cmake
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# - Try to find the Graphviz lib on Windows
+# - Try to find the ImageMagick lib on Windows
 #
 # This is a proxy module that calls the FindGraphviz.cmake module. Before
 # doing that, we try to guess where Graphviz might be on the user's machine.
@@ -20,18 +20,15 @@
 # the Graphviz directory can be found. The Graphviz directory must have "graphviz"
 # (case insensitive) in its name.
 #
-# This module helps the Windows user to avoid providing the following two
-# variables when building Zorba:
-# -D GRAPHVIZ_INCLUDE="path_to_3rd_party_dir\*graphviz*\src"
-# -D GRAPHVIZ_LIBRARY="path_to_3rd_party_dir\*graphviz*\bin\[Release]\graphviz.lib"
-#
 # See the FindLibTidy.cmake module shipped with Zorba for more information.
 
-FIND_PACKAGE_WIN32 (NAME Graphviz FOUND_VAR GRAPHVIZ_FOUND SEARCH_NAMES graphviz)
+FIND_PACKAGE_WIN32(NAME ImageMagick FOUND_VAR ImageMagick_FOUND SEARCH_NAMES ImageMagick COMPONENTS Magick++ MagickCore MagickWand)
 
-IF (GRAPHVIZ_FOUND)
+IF (ImageMagick_FOUND)
 
   #find the needed DLL's
-  FIND_PACKAGE_DLLS_WIN32 (${FOUND_LOCATION} graph.dll)
+  FIND_PACKAGE_DLLS_WIN32 (${FOUND_LOCATION} "config/colors.xml;config/coder.xml;config/configure.xml;config/delegates.xml;config/english.xml;config/locale.xml;config/log.xml;config/magic.xml;config/mime.xml;config/thresholds.xml;config/type.xml;config/type-ghostscript.xml;CORE_RL_tiff_.dll;CORE_RL_png_.dll;CORE_RL_libxml_.dll;CORE_RL_jbig_.dll;CORE_RL_jp2_.dll;CORE_RL_jpeg_.dll;CORE_RL_Magick++_.dll;CORE_RL_bzlib_.dll;CORE_RL_lcms_.dll;CORE_RL_ttf_.dll;CORE_RL_xlib_.dll;CORE_RL_zlib_.dll;CORE_RL_magick_.dll;CORE_RL_wand_.dll")
   
-ENDIF (GRAPHVIZ_FOUND)  
+ENDIF (ImageMagick_FOUND)  
+
+ 
